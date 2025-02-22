4cdef0f4a0554b8b0aed6b8e337d1770f4bd6305
Artem Melikhov <amkkaus@gmail.com>
2025-02-22 12:57:16 +0300

    Add files via upload

---
+++ Purple_4.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab_6.Purple_4;
+
+namespace Lab_6
+{
+    public class Purple_4
+    {
+        public struct Sportsman
+        {
+            private string _name, _surname;
+            private double _time;
+            private int _att; // only 1 time
+            
+            public string Name => _name;
+            public string Surname => _surname;
+            public double Time => _time;
+            public Sportsman (string name, string surname)
+            {
+                _name = name; _surname = surname;
+                _time = 0;
+                _att = 0;
+            }
+            public void Run (double time)
+            {
+                if (time < 0) return;
+                if (_att == 0) _time = time; _att++;
+            }
+            public void Print()
+            {
+
+            }
+        }
+        public struct Group
+        {
+            private string _name;
+            private Sportsman[] _sportsmen;
+
+            public string Name => _name;
+            public Sportsman[] Sportsmen
+            {
+                get
+                {
+                    if (_sportsmen == null || _sportsmen.Length == 0) return default;
+                    Sportsman[] copy = new Sportsman[_sportsmen.Length];
+                    Array.Copy(_sportsmen, copy, _sportsmen.Length);
+                    return copy;
+                }
+            }
+            public Group(string naming)
+            {
+                _name = naming;
+                _sportsmen = new Sportsman[] { };
+            }
+            public Group(Group elem)
+            {
+                _name = elem.Name;
+                if (elem.Sportsmen == null)
+                {
+                    _sportsmen = new Sportsman[0];
+                } else
+                {
+                    _sportsmen = new Sportsman[elem.Sportsmen.Length];
+                }
+                Array.Copy(elem.Sportsmen, _sportsmen, elem.Sportsmen.Length);
+            }
+            public void Add(Sportsman one_sportsman)
+            {
+                if (_sportsmen == null || _sportsmen.Length == 0) return;
+
+                Sportsman[] copy = new Sportsman[_sportsmen.Length + 1];
+                Array.Copy(_sportsmen, copy, _sportsmen.Length);
+                copy[copy.Length - 1] = one_sportsman;
+                _sportsmen = copy;
+            }
+            public void Add(Sportsman[] SPORTSmenGroup)
+            {
+                if (_sportsmen == null || _sportsmen.Length == 0 || SPORTSmenGroup == null) return;
+                Sportsman[] copy = new Sportsman[_sportsmen.Length + SPORTSmenGroup.Length];
+                Array.Copy(_sportsmen, copy, _sportsmen.Length);
+                Array.ConstrainedCopy(SPORTSmenGroup, 0, copy, _sportsmen.Length, SPORTSmenGroup.Length);
+                _sportsmen = copy;
+            }
+            public void Add_group(Group elem)
+            {
+                if (elem.Sportsmen == null || _sportsmen == null || _sportsmen.Length == 0) return;
+                Add(elem.Sportsmen);
+            }
+
+            public void Sort()
+            {
+                if (_sportsmen == null || _sportsmen.Length == 0) return;
+                Array.Sort(_sportsmen, (a,b) => {
+                    if (a.Time - b.Time > 0) return 1;
+                    else if (a.Time - b.Time < 0) return -1;
+                    else return 0;
+                    } );
+            }
+            public static Group Merge(Group group1, Group group2) {
+                Group copy = new Group("Финалисты");
+                copy.Add_group(group1);
+                copy.Add_group(group2);
+                return copy;
+            }
+            public void Print() { }
+        }
+    }
+}
---
+++ Purple_5.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab_6.Purple_5;
+
+namespace Lab_6
+{
+    public class Purple_5
+    {
+        public struct Response
+        {
+            private string _animal;
+            private string _charactertrait;
+            private string _concept;
+
+            public string Animal => _animal;
+            public string CharacterTrait => _charactertrait;
+            public string Concept => _concept;
+
+            public string[] Massive_of_answers // dop
+            {
+                get
+                {
+                    return new string[3] { _animal, _charactertrait, _concept };
+                }                          // 0 == 1     1 == 2                 2 == 3
+            }
+
+            public Response(string animal, string charactertrait, string concept)
+            {
+                _animal = animal; _charactertrait = charactertrait; _concept = concept;
+            }
+
+            public int CountVotes(Response[] responses, int questionNumber)
+            {
+                if (questionNumber < 1 || questionNumber > 3 || responses == null || responses.Length == 0) return default;
+                int count_question = 0;
+                int k = questionNumber;
+                k--;
+                foreach (Response response in responses)
+                {
+                    if (response.Massive_of_answers[k] != "-") count_question++;
+                }
+                return count_question;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{_animal} {_charactertrait} {_concept}");
+            }
+
+
+        }
+        public struct Research
+        {
+            private string _name; private Response[] _responses;
+
+            public string Name => _name;
+            public Response[] Responses
+            {
+                get
+                {
+                    if (_responses.Length == 0 || _responses == null) return default;
+
+                    Response[] copy_of_answers = new Response[_responses.Length];
+                    Array.Copy(_responses, copy_of_answers, _responses.Length);
+                    return copy_of_answers;
+                }
+            }
+
+            public Research(string name)
+            {
+                _name = name;
+                _responses = new Response[] { };
+            }
+            public void Add(string[] answers)
+            {
+                if (answers == null) return;
+
+                string[] new_answer = new string[3];
+
+                for (int i = 0; i < Math.Min(answers.Length, 3); i++)
+                {
+                    new_answer[i] = answers[i];
+                }
+
+                Response[] responses_1 = new Response[_responses.Length + 1];
+                Array.Copy(_responses, responses_1, _responses.Length);
+                responses_1[responses_1.Length - 1] = new Response(new_answer[0], new_answer[1], new_answer[2]);
+                // animal        char             conc
+                _responses = responses_1;
+            }
+
+            public string[] GetTopResponses(int question)
+            {
+                if (_responses == null) return default;
+                int question_num = question;
+                question_num -= 1;
+                int differ_question = 0;
+                for (int i = 0; i < _responses.Length; i++)
+                {
+                    int notrepeated = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (_responses[i].Massive_of_answers[question_num] == _responses[j].Massive_of_answers[question_num])
+                        {
+                            notrepeated++;
+                        }
+                    }
+                    differ_question = notrepeated == 0 ? differ_question += 1 : differ_question;
+                }
+
+
+                Resulting[] result_of_research = new Resulting[differ_question];
+
+                for (int i = 0; i < _responses.Length; i++)
+                {
+                    var response = _responses[i];
+
+                    for (int j = 0; j < differ_question; j++)
+                    {
+                        if (result_of_research[j].Count == 0)
+                        {
+                            result_of_research[j] = new Resulting(response.Massive_of_answers[question_num]);
+                            break;
+                        }
+                        else if (result_of_research[j].Meaning == response.Massive_of_answers[question_num])
+                        {
+                            result_of_research[j].Increasing_of_words();
+                            break;
+                        }
+                    }
+                }
+
+                Array.Sort(result_of_research, (a, b) =>
+                {
+                    return b.Count - a.Count;
+                });
+
+                int n = differ_question;
+                foreach (var a in result_of_research)
+                    if (a.Meaning == "-")
+                    {
+                        n--;
+                        break;
+                    }
+                string[] ans = new string[Math.Min(n, 5)];
+                int step = 0;
+                for (int i = 0; i < ans.Length; i++)
+                {
+                    if (result_of_research[i].Meaning == "-") step = 1;
+                    ans[i] = result_of_research[i + step].Meaning;
+                }
+                return ans;
+            }
+
+            private struct Resulting
+            {
+                private string _meaning;
+                private int _count;
+
+                public string Meaning => _meaning;
+                public int Count => _count;
+
+                public Resulting(string word)
+                {
+                    _meaning = word;
+                    _count = 1;
+                }
+
+                public void Increasing_of_words()
+                {
+                    _count++;
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(_name);
+                for (int i = 0; i < _responses.Length; i++)
+                {
+                    _responses[i].Print();
+                }
+            }
+        }
+    }
+}
+
+    
